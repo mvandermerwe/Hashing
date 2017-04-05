f47d22d3ea3fc6995bbd0fc6f8787803107f66ec
Mark Van der Merwe <vandermerwem2@gmail.com>
2017-04-05 13:12:54 -0600

    Crack brute force first go.

--- Assignment-10-Hashing-and-Passwords/src/cracking/Crack.java
+++ Assignment-10-Hashing-and-Passwords/src/cracking/Crack.java
@@ -3,12 +3,23 @@
  */
 package cracking;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+import hash_tables.Hash_Map;
 
 /**
  * Class that does several attempts to crack password files.
@@ -17,12 +28,60 @@ import java.util.concurrent.TimeUnit;
  */
 public class Crack {
 
+	private static Hash_Map<String, String> hashMap;
+
 	static public ArrayList<String> read_file_into_array(String file_name) {
-		return null;
+		BufferedReader bufferedReader = null;
+
+		try {
+			bufferedReader = new BufferedReader(new FileReader(file_name));
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found!");
+			System.exit(0);
+		}
+
+		ArrayList<String> passwords = new ArrayList<String>();
+		// Read a stream of strings from the file
+		Stream<String> hashWords = bufferedReader.lines();
+
+		hashWords.forEach(new Consumer<String>() {
+
+			@Override
+			public void accept(String t) {
+				// Add each string into the array list.
+				passwords.add(t);
+			}
+
+		});
+
+		return passwords;
 	}
 
 	static public HashSet<String> read_file_into_hash_set(String file_name) {
-		return null;
+		BufferedReader bufferedReader = null;
+
+		try {
+			bufferedReader = new BufferedReader(new FileReader(new File(file_name)));
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found!");
+			System.exit(0);
+		}
+
+		HashSet<String> passwords = new HashSet<String>();
+		// Read a stream of strings from the file
+		Stream<String> hashWords = bufferedReader.lines();
+
+		hashWords.forEach(new Consumer<String>() {
+
+			@Override
+			public void accept(String t) {
+				// Add each string into the hash set.
+				passwords.add(t);
+			}
+
+		});
+
+		return passwords;
 	}
 
 	/**
@@ -39,12 +98,17 @@ public class Crack {
 	 *         d077f244def8a70e5ea758bd8352fcd8AB3293292CEF2342ACD32342" ])
 	 */
 	static public ArrayList<String> brute_force_attack(Collection<String> hashes, int max_length) {
-		return null;
+		ArrayList<String> successes = new ArrayList<>();
+		// Let the helper handle the rest.
+		brute_force_attack(hashes, successes, new StringBuilder(), 1, max_length);
+		return successes;
 	}
 
 	/**
 	 * Finds all words of the provided length by recursively adding every
 	 * possible character onto the word.
+	 * 
+	 * TODO: Add timing stuff.
 	 * 
 	 * @param hashes
 	 *            - the collection of hashes to compare to to find password
@@ -66,7 +130,10 @@ public class Crack {
 			// System.out.println(so_far);
 			if (depth == max_length) {
 				// Hash and test.
-
+				String hashed = hash(so_far.toString()).toString();
+				if(hashes.contains(hashed)) {
+					successes.add("[ " + so_far + " : " + hashed + " ]");
+				}
 				// Delete last one so we can add the next letter.
 				so_far.deleteCharAt(so_far.length() - 1);
 			} else {
@@ -150,6 +217,28 @@ public class Crack {
 
 		return null;
 
+	}
+
+	private static StringBuffer hash(String value) {
+		MessageDigest hash_generator = null;
+		try {
+			hash_generator = java.security.MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println("Couldn't find Hash Generator algorithm.");
+			System.exit(0);
+		}
+
+		// build MD5 hash of a permutation
+		hash_generator.update(value.toString().getBytes());
+		byte[] digest = hash_generator.digest();
+
+		StringBuffer hashword_hex_code = new StringBuffer();
+		for (byte b : digest) {
+			hashword_hex_code.append(String.format("%02x", b & 0xff));
+		}
+
+		// use hashword_hex_code to compare to already encrypted/hashed words
+		return hashword_hex_code;
 	}
 
 }
