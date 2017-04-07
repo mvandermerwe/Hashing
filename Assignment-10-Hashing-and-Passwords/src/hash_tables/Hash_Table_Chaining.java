ac8051687162bdc3ac2996b6420f3968141f97d1
Andrew Haas <Andrew Haas@DESKTOP-EJCKAP0.uconnect.utah.edu>
2017-04-06 18:45:29 -0600

    Filled methods and commented.

--- Assignment-10-Hashing-and-Passwords/src/hash_tables/Hash_Table_Chaining.java
+++ Assignment-10-Hashing-and-Passwords/src/hash_tables/Hash_Table_Chaining.java
@@ -3,7 +3,10 @@
  */
 package hash_tables;
 
+import static hash_tables.Primes.next_prime;
+
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
  * Hash table implementation that uses MD5 hashing and probes collisions by
@@ -11,54 +14,165 @@ import java.util.ArrayList;
  * 
  * @author Mark Van der Merwe and Andrew Haas
  */
+
 public class Hash_Table_Chaining<KeyType, ValueType> implements Hash_Map<KeyType, ValueType> {
 
+	ArrayList<LinkedList<Pair<KeyType,ValueType>>> array = new ArrayList<LinkedList<Pair<KeyType,ValueType>>>();
+	protected int capacity;
+	protected int num_of_entries;
+	int collisions;
+	boolean resizeable;
+	
+	/**
+	 * Hash Table Chaining Constructor
+	 * 
+	 * @param initial_capacity
+	 *            - try to make this equal to twice the expected number of
+	 *            hashes
+	 */
+	public Hash_Table_Chaining(int initial_capacity) {
+		this.capacity = next_prime(initial_capacity);
+		init_table();
+		this.num_of_entries = 0;
+		collisions = 0;
+		resizeable=true;
+		for(int i = 0; i<capacity; i++){
+			array.add(new LinkedList<Pair<KeyType,ValueType>>());
+		}
+	}
+	
+	private void init_table() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public int wrap(int position){
+		return position%capacity;
+	}
+	/**
+	 * Creates a Pair out of the given key and value. Checks to
+	 * see if the key is already in the array of linked lists,
+	 * if it is overrides the old pair with the given one. if
+	 * it isn't in the array adds the pair to the corresponding
+	 * LinkedList. Counts all collisions that happen.
+	 * @param key
+	 * 			-The given KeyType
+	 * @param value
+	 * 			-The given ValueType		
+	 */
 	@Override
 	public void insert(KeyType key, ValueType value) {
-		// TODO Auto-generated method stub
-
+		Pair pair = new Pair(key, value);
+		num_of_entries++;
+		if(array.get(wrap(key.hashCode()))!=null){
+			
+		for(Pair<KeyType,ValueType> newpair: array.get(wrap(key.hashCode()))){
+			collisions++;
+			if(newpair.equals(pair)){
+				newpair=pair;
+				num_of_entries--;
+				return;
+			}
+		}
+		
+		}
+		array.get(wrap(key.hashCode())).add(pair);
+		
 	}
-
+	/**
+	 * This method checks if the array already has the 
+	 * given key in it and also count collisions.
+	 * @param key
+	 * 			-The given KeyType
+	 * @return -returns the value of the pair with the same key or returns null.
+	 */
 	@Override
 	public ValueType find(KeyType key) {
-		// TODO Auto-generated method stub
+		if(array.get(wrap(key.hashCode()))!=null){
+			for(Pair<KeyType,ValueType> newpair: array.get(wrap(key.hashCode()))){
+				collisions++;
+				if(newpair.key.equals(key)){
+					return newpair.value;
+				}
+			}
+		}
 		return null;
 	}
-
+	
+	/**
+	 * Resets all variables except capacity and clears the array
+	 */
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-
+		init_table();
+		this.num_of_entries = 0;
+		collisions = 0;
+		for(int i = 0; i<capacity; i++){
+			array.set(i, new LinkedList<Pair<KeyType,ValueType>>());
+		}
 	}
-
+	
+	/**
+	 * Returns the capacity of the array.
+	 * @return this.capacity
+	 */
 	@Override
 	public int capacity() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.capacity;
 	}
-
+	
+	/**
+	 * Returns the number of elements in the array.
+	 * @return this.num_of_entries
+	 */
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.num_of_entries;
 	}
 
+	/**
+	 * Sets the resizeable instance variable to the given boolean.
+	 * @param status
+	 * 			-true/false of whether or not the array can be resized
+	 */
 	@Override
 	public void set_resize_allowable(boolean status) {
-		// TODO Auto-generated method stub
+		resizeable=status;
 
 	}
 
+	/**
+	 * Returns an array of doubles containing the average collisions,
+	 * number of entries, and the capacity of the array.
+	 * @return stats	-array containing the previously stated data.
+	 */
 	@Override
 	public ArrayList<Double> print_stats() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Double> stats = new ArrayList<Double>();
+		stats.add((double)collisions/num_of_entries);
+		stats.add((double)num_of_entries);
+		stats.add((double)capacity);
+		return stats;
 	}
-
+	
+	/**
+	 * Replaces array for a new one of the given size and copies the
+	 * LinkedLists over.
+	 * @param new_size
+	 * 			-int representing the size of the new array
+	 */
 	@Override
 	public void resize(int new_size) {
-		// TODO Auto-generated method stub
-
+		ArrayList<LinkedList<Pair<KeyType,ValueType>>> temparray = array;
+		array= new ArrayList<LinkedList<Pair<KeyType,ValueType>>>();
+		capacity=new_size;
+		for(int i = 0; i<capacity; i++){
+			array.add(new LinkedList<Pair<KeyType,ValueType>>());
+		}
+		for(int i = 0; i<temparray.size();i++){
+			array.set(i, temparray.get(i));
+		}
+		
 	}
 
 }
