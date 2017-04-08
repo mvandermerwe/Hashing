9354b62cf069fe8f3687e465965febe7b0aff864
Andrew Haas <Andrew Haas@DESKTOP-EJCKAP0.uconnect.utah.edu>
2017-04-07 21:37:31 -0600

    started analysis

--- Assignment-10-Hashing-and-Passwords/src/cracking/Testing.java
+++ Assignment-10-Hashing-and-Passwords/src/cracking/Testing.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 
 import hash_tables.Hash_Map;
 import hash_tables.Hash_Table_Chaining;
+import hash_tables.Hash_Table_Linear_Probing;
+import hash_tables.Hash_Table_Quadtratic_Probing;
 
 /**
  * Class to encapsulate all of our testing stuff.
@@ -16,15 +18,15 @@ import hash_tables.Hash_Table_Chaining;
 public class Testing {
 	
 	public void main(String[] args) {
-		ArrayList<String> array=Crack.read_file_into_array("Resources/a_few_hashes");
+		ArrayList<String> array=Crack.read_file_into_array("Resources/names");
 		// Change which is commented to test different implementations of
-		Hash_Map<String, Integer> hashMap;
-		//hashMap = new Hash_Table_Linear_Probing<String, Integer>(79);
-		// hashMap = new Hash_Table_Quadtratic_Probing<String, Integer>(79);
-		hashMap = new Hash_Table_Chaining<String, Integer>(79);
+		Hash_Map<String, Integer> hashMap = new Hash_Table_Linear_Probing<String, Integer>(1000);
+		Hash_Map<String, Integer> hashMap2= new Hash_Table_Quadtratic_Probing<String, Integer>(1000);
+		Hash_Map<String, Integer>	hashMap3 = new Hash_Table_Chaining<String, Integer>(1000);
 		hashMap.set_resize_allowable(false);
-		for(int i = 0; i<79;i++){
-			hashMap.insert(array.get(i), i);
+		for(int i = 0; i<array.size();i++){
+			hashMap.insert(array.get(i)+array.get(i+1), Integer.parseInt(array.get(i+2)));
+			hashMap2.insert(array.get(i)+array.get(i+1), Integer.parseInt(array.get(i+2)));
 		}
 	}
 
